b83e7fb49055ff285182d2b8214b9a8c5087cae0
Felix.Fire619 <felix.fire619@gmail.com>
2016-01-24 23:14:55 -0800

    Fixed HTTP Output

--- web.js
+++ web.js
@@ -11,15 +11,13 @@ var fs   = require('fs');
 
 const PORT=8080;
 
-var data = fs.readFile('public/messages.html', 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
-
 function handleRequest(request, response){
-    response.end(data);
-}
-
+    fs.readFile('public/messages.html', 'utf-8', (err, data) => {
+  if (err) throw err;
+  response.end(data);
+});
+};
+    
 var server = http.createServer(handleRequest);
 
 server.listen(PORT, function(){
